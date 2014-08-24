55161f8e1521f13202273aee0b8320302e1561be
palitchatokey <pali2002fr@yahoo.fr>
2014-08-24 19:11:04 -0400

    fix CORS issues7

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && (   
+       $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST' || 
+       $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'DELETE' || 
+       $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'PUT' )) {
+             header('Access-Control-Allow-Origin: *');
+             header("Access-Control-Allow-Credentials: true"); 
+             header('Access-Control-Allow-Headers: X-Requested-With');
+             header('Access-Control-Allow-Headers: Content-Type');
+             header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); // http://stackoverflow.com/a/7605119/578667
+             header('Access-Control-Max-Age: 86400'); 
+      }
+  exit;
+}
 /**
  * Step 1: Require the Slim Framework
  *
@@ -97,12 +108,6 @@ $app->get('/users', function() use ($userMapper, $app) {
             $users[$key]['id'] = $value->getId();
             $users[$key]['name'] = $value->getName();
         }
-
-        $app->response()->status(200);
-        $app->response()->header('Access-Control-Allow-Origin', '*'); 
-        $app->response()->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, X-authentication, X-client');
-        $app->response()->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
         $app->response()->header("Content-Type", "application/json");
         echo '{"user": ' . json_encode($users) . '}';
     } catch(PDOException $e) {
